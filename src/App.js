98408ff80c9800e1a4f29d825969ffe680514959
zgulen <gulenzubeyir@gmail.com>
2022-06-20 18:15:07 +0300

    useState- useEffect and intervarl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // const [showComponent, setShowComponent] = useState(true)
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [timer, setTimer] = useState(0);
+  useEffect( () =>{
+    const myTimer = setInterval(()=>{
+      setTimer(timer => timer + 1)
+    } , 1000)
+    return () => clearInterval(myTimer)
+  }, [])
+  useEffect(()=>{
+    console.log(timer);
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <p>You clicked {count} times</p>
+      <p>You clicked {count2} times</p>
+      <p>Second: {timer}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <button onClick={() => setCount2(count2 + 1)}>
+        Click me 2
+      </button>
     </div>
+
   );
-}
+  }
 
 export default App;
